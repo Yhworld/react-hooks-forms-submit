16d177b80bfa0a56249e9d2820b1ad2cbcc4a463
Yahya <yahyahussein670@gmail.com>
2022-07-18 13:15:00 +0300

    commit tree 274b5cf0ccb29dea2c43410bf8abd0757b8dd78e

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 function Form(props) {
   const [firstName, setFirstName] = useState("Sylvia");
   const [lastName, setLastName] = useState("Woods");
+  const [nameData, setNameData] = useState([])
 
   function handleFirstNameChange(event) {
     setFirstName(event.target.value);
@@ -11,13 +12,31 @@ function Form(props) {
   function handleLastNameChange(event) {
     setLastName(event.target.value);
   }
-
+  function handleSubmit(e) {
+    e.preventDefault()
+    const formData = {firstName, lastName}
+    const getThat = [...nameData, formData]
+    setNameData(getThat);
+    setFirstName("");
+    setLastName("");
+  }
+  const fetchData = nameData.map((data) => {
+    return (
+      <div>
+        <span style={{ color: "green" }}>{data.firstName} {data.lastName}</span>
+      </div>
+    )
+  })
   return (
+  <div>
     <form>
       <input type="text" onChange={handleFirstNameChange} value={firstName} />
       <input type="text" onChange={handleLastNameChange} value={lastName} />
-      <button type="submit">Submit</button>
+      <button type="submit" onClick={handleSubmit}>Submit</button>
     </form>
+    <h1>headerrrrr!!!!!</h1>
+    {fetchData}
+  </div>
   );
 }
 
